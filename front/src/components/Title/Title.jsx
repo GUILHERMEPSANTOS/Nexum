8a3f76f4b5e69035897d56251787c1883b116d72
AnaCMR <ana.silva@bandtec.com.br>
2022-09-29 20:45:01 -0300

    Refactor: title

--- front/src/components/Title/Title.jsx
+++ front/src/components/Title/Title.jsx
@@ -1,25 +1,32 @@
-import { useEffect, useState } from "react";
+import { WORDS } from "./constants";
+import watchWords from "./hooks";
 import styles from "./styles.module.scss";
 
 const Title = ({ text }) => {
-  const [words, setWords] = useState([]);
-
-  useEffect(() => {
-    const teste = text.split(" ");
-    setWords(teste);
-  }, []);
+  const { words } = watchWords({ text });
 
   return (
-    <h1 className={styles.title}>
-      {words.map((item) => {
-        const color =
-          item == "Nexum" ||
-          item == "conexão" ||
-          item == "Freelancers" ||
-          item == "avaliados"
-            ? "#7509BD"
-            : "#fff";
-        return <span style={{ color: `${color}` }}>{item + ` `}</span>;
+    <h1 className={styles.container}>
+      {words.map((item, i) => {
+        const teste = {
+          item: "#7509BD",
+        };
+
+        const color = teste[item] || "#FFF";
+
+        console.log(teste);
+        console.log(color);
+        console.log(teste[item]);
+        console.log(item);
+        return (
+          <h1
+            key={`word-${i}`}
+            className={styles.title}
+            style={{ color: `${color}` }}
+          >
+            {item + ` `}
+          </h1>
+        );
       })}
     </h1>
   );
