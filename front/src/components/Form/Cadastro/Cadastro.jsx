e8bccc41c64619b5ce27ff3508ea25524e33a362
AnaCMR <ana.silva@bandtec.com.br>
2022-10-07 20:57:03 -0300

    feat: deixando as inputs funcionais

--- front/src/components/Form/Cadastro/Cadastro.jsx
+++ front/src/components/Form/Cadastro/Cadastro.jsx
@@ -2,34 +2,75 @@ import Button from "../../Buttons/Button";
 import Title from "../../Title/Title";
 import Text from "../../Text/Text";
 
-import styles from "./styles.module.scss"
+import styles from "./styles.module.scss";
 import { useState } from "react";
 
 const Cadastro = () => {
-const [passwordType, setPasswordType] = useState();
+  const [user, setUser] = useState();
+  const [cellphone, setCellphone] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
+  const [passwordType, setPasswordType] = useState("password");
+  const [type, setType] = useState("password");
 
   return (
     <section className={styles.container}>
-    <Title text="Crie uma conta"/>
-    <Text text="Realize o cadastro para se conectar"/>
-    <div className={styles.content}>
-    <label className={styles.labels}>Usuário</label>
-    <input/>
-    <label  className={styles.labels}>Celular</label>
-    <input/>
-    <label  className={styles.labels}>E-mail</label>
-    <input/>
-    <label  className={styles.labels}>Senha</label>
-    <input type={passwordType}/>
-    <label  className={styles.labels}>Confirmar senha</label>
-    <input type={passwordType}/>
-    </div>
-    <div className={styles.buttons}>
-    <Button text="Entrar"/>
-    <Button  isEmpty={true} text="Já possui uma conta?"/>
-    </div>
+      <Title text="Crie uma conta" />
+      <Text text="Realize o cadastro para se conectar" />
+      <div className={styles.content}>
+        <label className={styles.labels}>Usuário</label>
+        <input value={user} onChange={({ target }) => setUser(target.value)} />
+        <label className={styles.labels}>Celular</label>
+        <input
+          value={cellphone}
+          onChange={({ target }) => setCellphone(target.value)}
+        />
+        <label className={styles.labels}>E-mail</label>
+        <input
+          value={email}
+          onChange={({ target }) => setEmail(target.value)}
+        />
+        <label className={styles.labels}>Senha</label>
+        <div className={styles.password}>
+          <input
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+            type={passwordType}
+          />
+          <img
+            onClick={() =>
+              passwordType == "text" ? setType("password") : setType("text")
+            }
+            src={`../../../../assets/icons/${
+              passwordType == "text" ? "unsee" : "see"
+            }.svg`}
+            alt="ver senha"
+          />
+        </div>
+        <label className={styles.labels}>Confirmar senha</label>
+        <div className={styles.password}>
+          <input
+            value={confirmPassword}
+            onChange={({ target }) => setConfirmPassword(target.value)}
+            type={type}
+          />
+          <img
+            onClick={() =>
+              type == "text" ? setType("password") : setType("text")
+            }
+            src={`../../../../assets/icons/${
+              type == "text" ? "unsee" : "see"
+            }.svg`}
+            alt="ver senha"
+          />
+        </div>
+      </div>
+      <div className={styles.buttons}>
+        <Button text="Entrar" />
+        <Button isEmpty={true} text="Já possui uma conta?" />
+      </div>
     </section>
-
   );
 };
 
