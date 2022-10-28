47200f482ec63889aadcd06e71a90c9329e33a05
AnaCMR <ana.silva@bandtec.com.br>
2022-10-27 21:29:39 -0300

    feat: algo

--- front/src/components/Profile/List/List.jsx
+++ front/src/components/Profile/List/List.jsx
@@ -2,21 +2,24 @@ import Text from "../../Text/Text";
 import Title from "../../Title/Title";
 import styles from "./styles.module.scss";
 
-const List = ({ title = "", list }) => (
-  <section className={styles.container}>
-    <Title text={title} />
-    <ul className={styles.listItemsContainer}>
-      {list.map(({ name, icon, date, number, rate, text }) => (
-        <li className={styles.listItems}>
-          {!!!icon && <img src={`../../assets/icons/${icon}.svg`} />}
-          {!!!name && <Text isSmall={true} text={name} />}
-          {!!!date && <Text isSmall={true} text={date} />}
-          {!!!rate && <Text isSmall={true} text={number} />}
-          {!!!text && <Text isSmall={true} text={rate} />}
-          {!!!number && <Text isSmall={true} text={text} />}
-        </li>
-      ))}
-    </ul>
-  </section>
-);
+const List = ({ title = "", list }) => {
+  console.log(title, list);
+  return (
+    <section className={styles.container}>
+      <Title text={title} />
+      <ul className={styles.listItemsContainer}>
+        {list.map(({ name, icon, date, number, rate, text }) => (
+          <li className={styles.listItems}>
+            {!!!icon && <img src={`../../assets/icons/${icon}.svg`} />}
+            {!!!name && <Text isSmall={true} text={name} />}
+            {!!!date && <Text isSmall={true} text={date} />}
+            {!!!rate && <Text isSmall={true} text={number} />}
+            {!!!text && <Text isSmall={true} text={rate} />}
+            {!!!number && <Text isSmall={true} text={text} />}
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
 export default List;
