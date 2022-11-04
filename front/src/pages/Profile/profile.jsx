64826f5c7fa842b0b93ed9aaa0acb2fd02b2f2ca
Abel-JACN <ABEL.NETO@bandtec.com.br>
2022-11-03 21:01:10 -0300

    feat: tela favorito

--- front/src/pages/Profile/profile.jsx
+++ front/src/pages/Profile/profile.jsx
@@ -3,11 +3,13 @@ import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import Company from "../../components/Profile/Pages/Company/Company";
 import Freelancer from "../../components/Profile/Pages/Freelancer/Profile";
+import FavoritosMain from "../../components/TelaFavoritos/Favoritos"
 
 const Profile = ({ isCompanyProfile = false }) => (
   <>
     <Header options={OPTIONS} buttonText="Sair" link="/" />
-    {isCompanyProfile ? <Company /> : <Freelancer />}
+    {/* {isCompanyProfile ? <Company /> : <Freelancer />} */}
+    <FavoritosMain />
     <Footer />
   </>
 );
