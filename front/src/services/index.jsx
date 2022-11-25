cdbe6fa72224a9e5e0592b8a1d7b309243746f53
AnaCMR <ana.silva@bandtec.com.br>
2022-11-25 19:52:08 -0300

    feat: diferença cadastro

--- front/src/services/index.jsx
+++ front/src/services/index.jsx
@@ -12,7 +12,7 @@ export async function APILogin({ email, senha }) {
   );
 }
 
-export function APICadastro({ email, senha, nome, celular }) {
+export function APICadastroFreela({ email, senha, nome, celular }) {
   axios
     .post(
       "http://localhost:8080/api/v1/controle-acesso/create-account/freelancer",
@@ -23,6 +23,17 @@ export function APICadastro({ email, senha, nome, celular }) {
         celular: celular,
       }
     )
+    .then((response) => displayOutput(response))
+    .catch((err) => console.log(err));
+}
+export function APICadastroContratante({ email, senha, nome, celular }) {
+  axios
+    .post("http://localhost:8080/api/v1/contratante/create-account/", {
+      email: email,
+      senha: senha,
+      nome: nome,
+      celular: celular,
+    })
     .then((response) => displayOutput(response))
     .catch((err) => console.log(err));
 }
