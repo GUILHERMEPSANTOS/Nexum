d57378e6f508284bbb10ec8beb958c282388ec92
AnaCMR <ana.silva@bandtec.com.br>
2022-11-25 22:27:57 -0300

    feat: add like option

--- front/src/components/Profile/Inicio/constants.jsx
+++ front/src/components/Profile/Inicio/constants.jsx
@@ -1,7 +1,23 @@
-export const TEXTS = [
+export const TEXTS_FREELANCER = [
   {
     title: "Clique aqui para visualizar ",
     text: "Propostas",
+    link: "/propostas",
+  },
+  {
+    title: "Clique aqui para visualizar",
+    text: "Perfil",
+    link: "/perfil",
+  },
+  {
+    title: "Clique aqui para entrar em ",
+    text: "Contato",
+  },
+];
+export const TEXTS_CONTRATANTE = [
+  {
+    title: "Clique aqui para visualizar ",
+    text: "Freelancers",
     link: "/escolha-freelancer",
   },
   {
--- front/src/pages/ProfilePages/FreelancersChoose/FreelancersChoose.jsx
+++ front/src/pages/ProfilePages/FreelancersChoose/FreelancersChoose.jsx
@@ -4,6 +4,8 @@ import CardWithInfo from "../../../components/Cards/CardsBanner/CardBanner/CardW
 import ProfileContainer from "../profile";
 import { DATA } from "./Constants";
 import styles from "./styles.module.scss";
+import List from "../../../components/Profile/List/List";
+import Text from "../../../components/Text/Text";
 
 const FreelancerChoose = () => {
   const [openModal, setOpenModal] = useState(true);
@@ -14,7 +16,22 @@ const FreelancerChoose = () => {
           <div className={styles.cardContainer}>
             <CardWithInfo data={DATA} />
           </div>
-          <div className={styles.tooltip}></div>
+          <div className={styles.cardContainerInfo}>
+            {DATA.map(({ sobre, habilidades }) => (
+              <div>
+                <div className={styles.actions}>
+                  <button>
+                    <img src="../../assets/icons/like.svg" />
+                  </button>
+                  <button>
+                    <img src="../../assets/icons/save.svg" />
+                  </button>
+                </div>
+                <Text isSmall={true} text={sobre} />
+                <List list={habilidades} />
+              </div>
+            ))}
+          </div>
         </div>
       </ProfileContainer>
       <CreateOffer actualState={openModal} setActualState={setOpenModal} />
