4db33f3a52de36dbbbeb69fd30dfe487ef8aaf46
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2022-11-26 22:46:21 -0300

    implementação do match

--- front/src/pages/ProfilePages/FreelancersChoose/FreelancersChoose.jsx
+++ front/src/pages/ProfilePages/FreelancersChoose/FreelancersChoose.jsx
@@ -1,7 +1,8 @@
-import { useCallback, useState } from "react";
+import { useCallback, useState, useMemo } from "react";
 
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { getFreelancers } from "../../../services/Freelancer/freelancer";
+import { sendMatchRequest } from "../../../services/Freelancer/match/freelancer";
 
 import CreateOffer from "../../../components/Modals/CreateOffer/CreateOffer";
 import CardWithInfo from "../../../components/Cards/CardsBanner/CardBanner/CardWithInfo";
@@ -10,59 +11,72 @@ import ProfileContainer from "../profile";
 import Text from "../../../components/Text/Text";
 
 import styles from "./styles.module.scss";
-import { postMatch } from "../../../services/Freelancer/match/freelancer";
 
 const FreelancerChoose = () => {
   const [openModal, setOpenModal] = useState(true);
-  // const [id, setId] = useState();
 
-  const idContratante = localStorage.getItem("user_id");
+  const idContratante = useMemo(() => localStorage.getItem("user_id"));
 
   const { data, isLoading } = useQuery(["consultar freelancers"], () =>
     getFreelancers()
   );
 
-  async function match(id) {
-    await postMatch({
-      id_contratante: idContratante,
-      id_freelancer: id,
-    });
-  }
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <>
-      <ProfileContainer>
-        <div className={styles.cardWrapper}>
-          <div className={styles.cardContainer}>
-            <CardWithInfo data={data?.data ?? []} />
-          </div>
-          <div className={styles.cardContainerInfo}>
-            {data?.data?.map(({ nome, id_user }, i) => (
-              <div key={i}>
-                <div className={styles.actions}>
-                  <button onClick={() => match(id_user)}>
-                    <img src="../../assets/icons/like.svg" />
-                  </button>
-                  <button>
-                    <img src="../../assets/icons/save.svg" />
-                  </button>
-                </div>
-                <Text isSmall={true} text={nome} />
-
-                <Text isSmall={true} text={"Ver perfil"} />
-                {/* <List list={habilidades} /> */}
-              </div>
-            ))}
-          </div>
-        </div>
-      </ProfileContainer>
-      {/* <CreateOffer actualState={openModal} setActualState={setOpenModal} /> */}
-    </>
+  const { mutate } = useMutation(({ id_freelancer, id_contratante }) =>
+    sendMatchRequest({ id_freelancer, id_contratante }),
+    {
+      onSuccess: () => {
+        alert("Sucesso")
+      },
+      onError: () => {
+        alert("Error");
+      }
+    }
   );
+
+const handleSubmit = useCallback(
+  (id_freelancer) => {
+    mutate({
+      id_freelancer,
+      id_contratante: Number(idContratante),
+    });
+  },
+  [idContratante, sendMatchRequest]
+);
+
+if (isLoading) {
+  return <div>Loading...</div>;
+}
+
+return (
+  <>
+    <ProfileContainer>
+      <div className={styles.cardWrapper}>
+        <div className={styles.cardContainer}>
+          <CardWithInfo data={data?.data ?? []} />
+        </div>
+        <div className={styles.cardContainerInfo}>
+          {data?.data?.map(({ nome, id_user }, i) => (
+            <div key={i}>
+              <div className={styles.actions}>
+                <button onClick={() => handleSubmit(id_user)}>
+                  <img src="../../assets/icons/like.svg" />
+                </button>
+                <button>
+                  <img src="../../assets/icons/save.svg" />
+                </button>
+              </div>
+              <Text isSmall={true} text={nome} />
+
+              <Text isSmall={true} text={"Ver perfil"} />
+              {/* <List list={habilidades} /> */}
+            </div>
+          ))}
+        </div>
+      </div>
+    </ProfileContainer>
+    {/* <CreateOffer actualState={openModal} setActualState={setOpenModal} /> */}
+  </>
+);
 };
 
 export default FreelancerChoose;
