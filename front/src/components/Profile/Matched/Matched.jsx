3479714836f7c886487f86b2696d3fbfc01524fe
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2022-11-27 19:10:56 -0300

    chamada da api

--- front/src/components/Profile/Matched/Matched.jsx
+++ front/src/components/Profile/Matched/Matched.jsx
@@ -1,14 +1,31 @@
-import Company from "../../../pages/Profile/Company/Company";
+import { useMemo } from "react";
+
+import { useQuery } from "@tanstack/react-query";
+import { getMatchsRequestByFreelancerId } from "../../../services/Freelancer/match/freelancer";
+
 import { Link } from "react-router-dom";
+
+import Company from "../../../pages/Profile/Company/Company";
+
 const Matched = () => {
+  const userId = useMemo(() => localStorage.getItem("user_id"));
+
+  const { data, isLoading } = useQuery(
+    ["consultar solicitações de match"],
+    () => getMatchsRequestByFreelancerId({ id_freelancer: userId })
+  );
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <>
-      oi
-      {/* {list.map(() => (
-        <Link to={'/propostas'}>
-        <Company isFreela={true} />
+      {/* {data?.data.map(() => (
+        <Link to={"/propostas"}>
+          <Company isFreela={true} />
         </Link>
-      ))} */}
+      ))}  */}
     </>
   );
 };
