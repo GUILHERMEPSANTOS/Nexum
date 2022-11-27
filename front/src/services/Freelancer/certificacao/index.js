d6dccc1bf47fe69ffe516a2c0a95e644f2f4b7f5
AnaCMR <ana.silva@bandtec.com.br>
2022-11-26 22:00:14 -0300

    fix: removing data

--- front/src/services/Freelancer/certificacao/index.js
+++ front/src/services/Freelancer/certificacao/index.js
@@ -3,7 +3,7 @@ import { api } from "../../api";
 const listCertificadoByFreelancerId = async (id) => {
   const response = await api.get(`/api/v1/certificacao/list/freelancer/${id}`);
 
-  return response.data;
+  return response;
 };
 
-export { listCertificadoByFreelancerId }
+export { listCertificadoByFreelancerId };
