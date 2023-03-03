5fbc5b9abb8704c760572aae673f9215ebc8c25c
AnaCMR <ana.silva@bandtec.com.br>
2023-03-03 19:26:43 -0300

    feat: frelancer

--- front/src/components/TypeUser/TypeUser.jsx
+++ front/src/components/TypeUser/TypeUser.jsx
@@ -5,53 +5,31 @@ import Button from "../Buttons/Button";
 import styles from "./styles.module.scss";
 
 const UserMain = () => {
-    return (
-
-        <Banner>
-
-            <div className={styles.container}>
-                <div className={styles.saiba_mais}>
-                    <Text
-                        text="CRIE AGORA MESMO A SUA CONTA"
-                    />
-                    <Title
-                        text="Encontre o melhor da Nexum"
-                    />
-                    <Text
-                        text="Conecte-se a pessoas que buscam crescer no mercado e freelancers que desejam ser vistos pelas suas habilidades"
-                    />
-                    
-                </div>
-                <div className={styles.box_conta}>
-                    <Title
-                        text="Frellancer"
-                    />
-                    <Text
-                        text="Para você que deseja encontrar o trabalho perfeito "
-                    />
-                    <div className={styles.buttons}>
-                        <Button
-                            text="Conta Freelancer"
-                        />
-                    </div>
-                </div>
-                <div className={styles.box_conta}>
-                    <Title
-                        text="Contratante"
-                    />
-                    <Text
-                        text="Para você que busca encontrar os melhores pofissionais"
-                    />
-                    <div className={styles.buttons}>
-                        <Button
-                            text="Conta Contratante" isEmpty={true}
-                        />
-                    </div>
-                </div>
-            </div>
-        </Banner>
-
-    )
-}
+  return (
+    <Banner>
+      <div className={styles.container}>
+        <div className={styles.saiba_mais}>
+          <Text text="CRIE AGORA MESMO A SUA CONTA" />
+          <Title text="Encontre o melhor da Nexum" />
+          <Text text="Conecte-se a pessoas que buscam crescer no mercado e freelancers que desejam ser vistos pelas suas habilidades" />
+        </div>
+        <div className={styles.box_conta}>
+          <Title text="Frelancer" />
+          <Text text="Para você que deseja encontrar o trabalho perfeito " />
+          <div className={styles.buttons}>
+            <Button text="Conta Freelancer" />
+          </div>
+        </div>
+        <div className={styles.box_conta}>
+          <Title text="Contratante" />
+          <Text text="Para você que busca encontrar os melhores pofissionais" />
+          <div className={styles.buttons}>
+            <Button text="Conta Contratante" isEmpty={true} />
+          </div>
+        </div>
+      </div>
+    </Banner>
+  );
+};
 
 export default UserMain;
