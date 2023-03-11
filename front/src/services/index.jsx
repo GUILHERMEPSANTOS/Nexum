38d16092ba1ce449374b821f90abf219f331f7f1
AnaCMR <ana.silva@bandtec.com.br>
2023-03-10 21:30:47 -0300

    feat: change api routes

--- front/src/services/index.jsx
+++ front/src/services/index.jsx
@@ -23,12 +23,15 @@ export function APICadastroFreela({ email, senha, nome, celular }) {
 }
 export function APICadastroContratante({ email, senha, nome, celular }) {
   axios
-    .post("http://localhost:8000/api/v1/contratante/create-account/", {
-      email: email,
-      senha: senha,
-      nome: nome,
-      celular: celular,
-    })
+    .post(
+      "ec2-44-212-184-96.compute-1.amazonaws.com:8000/api/v1/contratante/create-account/",
+      {
+        email: email,
+        senha: senha,
+        nome: nome,
+        celular: celular,
+      }
+    )
     .then((response) => displayOutput(response))
     .catch((err) => console.log(err));
 }
