0080965bf22b92d2dbd8ea15ef2b943b8bef5657
ViniciusPieroni <vinizanetini@gmail.com>
2023-03-28 19:36:26 -0300

    Nexum Project AppMb

---
+++ NexumProject/app/src/main/java/com/example/nexumproject/TelaDePerfilFreelancer.kt
@@ -0,0 +1,11 @@
+package com.example.nexumproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TelaDePerfilFreelancer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_de_perfil_freelancer)
+    }
+}
