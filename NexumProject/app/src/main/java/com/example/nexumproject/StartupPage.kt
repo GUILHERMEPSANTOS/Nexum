2faea8bd00122b55f14557f439db0675c28feab1
ViniciusPieroni <vinizanetini@gmail.com>
2023-03-30 00:06:33 -0300

    novas telas do app

---
+++ NexumProject/app/src/main/java/com/example/nexumproject/StartupPage.kt
@@ -0,0 +1,11 @@
+package com.example.nexumproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class StartupPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_startup_page)
+    }
+}
