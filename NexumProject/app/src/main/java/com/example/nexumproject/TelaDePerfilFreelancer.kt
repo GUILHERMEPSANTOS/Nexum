b02e6b7400ec8f5fb762df9af2cd47ac668a429e
ViniciusPieroni <vinizanetini@gmail.com>
2023-04-24 11:44:25 -0300

    paginas app nexum

--- NexumProject/app/src/main/java/com/example/nexumproject/TelaDePerfilFreelancer.kt
+++ NexumProject/app/src/main/java/com/example/nexumproject/TelaDePerfilFreelancer.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 
 
 class TelaDePerfilFreelancer : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_de_perfil_freelancer)
