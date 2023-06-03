53ab0350c70d3182fbad10758001523e2c422776
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-06-03 19:59:15 -0300

    fix: correção problema de segurança

--- backend/src/main/java/com/nexum/backend/BackendApplication.java
+++ backend/src/main/java/com/nexum/backend/BackendApplication.java
@@ -2,10 +2,10 @@ package com.nexum.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class BackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
