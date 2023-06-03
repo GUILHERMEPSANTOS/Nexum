44660c850a77404395a3995a90443dda1ca5872b
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-06-03 19:52:29 -0300

    fix: correção bug iniciação

--- backend/src/main/java/com/nexum/backend/BackendApplication.java
+++ backend/src/main/java/com/nexum/backend/BackendApplication.java
@@ -2,12 +2,11 @@ package com.nexum.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
 
+@SpringBootApplication
 public class BackendApplication {
-	public static void main(String[] args) throws Exception {
+
+	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 }
