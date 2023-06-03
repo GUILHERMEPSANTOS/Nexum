ccf7edf7394a418eb46a2753868721352cf71196
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-06-03 17:42:19 -0300

    fix: voltando

--- front/src/services/api.js
+++ front/src/services/api.js
@@ -1,11 +1,7 @@
 import axios from "axios";
-import https from "https";
 
 const api = axios.create({
   baseURL: "http://44.212.184.96:8000",
-  httpsAgent: new https.Agent({
-    rejectUnauthorized: false
-  })
 });
 
 export { api };
