4e4a7774d83318287bd78154844cf081794f63f2
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-06-03 17:32:04 -0300

    refactor: ignore https requests

--- front/src/services/api.js
+++ front/src/services/api.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://44.212.184.96:8000",
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false
+  })
 });
 
 export { api };
